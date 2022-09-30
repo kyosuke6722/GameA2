6d4247ab12d36717474900784462fdc618a930e3
kyosuke483 <108373237+kyosuke483@users.noreply.github.com>
2022-09-30 12:44:42 +0900

    Game03/UI03.cpp進めました

--- Project/GameProject/Game03/UI03.cpp
+++ Project/GameProject/Game03/UI03.cpp
@@ -1,8 +1,16 @@
 #include "UI03.h"
+#include"GameData03.h"
 
 UI03::UI03():Base(eType_UI) {
 	m_img = COPY_RESOURCE("UI03", CImage);
 }
 
-void UI03::Draw(){
+void UI03::Draw() {
+	int time = GameData03::t_time/60;
+	for (int i = 0; i < 4; i++,time/10) {
+		int t = time % 10;
+		m_img.SetRect(16*t,16,16*t+16,32);
+		m_
+
+	}
 }
