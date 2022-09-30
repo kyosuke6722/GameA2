f60c69008a205d0d3d74ac2800b5fdad0c8bc660
ryou-anubuki <A2oo3o812r>
2022-09-30 12:43:18 +0900

    a

---
+++ Project/GameProject/Game01/GameData01.cpp
@@ -0,0 +1,5 @@
+#include"GameData01.h"
+
+int GameData01::point[2] = { 0,0 };
+
+
---
+++ Project/GameProject/Game01/UI01.cpp
@@ -0,0 +1,34 @@
+#include "UI01.h"
+#include"GameData01.h"
+
+
+
+
+UI01::UI01()
+	:Base(eType_UI)
+{
+	m_score = COPY_RESOURCE("Number",CImage);
+	m_time = COPY_RESOURCE("UI", CImage);
+}
+
+
+
+void UI01::Draw()
+{
+	int base_x[2] = { 640 - 48.640 + 48 };
+	for (int j = 0; j < 2; j++) {
+		int n = GameData01 :: point[j];
+
+	
+		for (int i = 0; i < 2; i++, n /= 10) {
+			int p = n % 10;
+
+			m_score.SetRect(48 * p, 48 * j, 48 * (p + 1), 48 * (j + 1));
+
+			m_score.SetSize(48, 48);
+			m_score.SetPos(base_x[j] - i * 48, 0);
+			m_score.Draw();
+
+		}
+
+}
