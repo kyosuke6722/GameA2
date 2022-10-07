ab2ec28e594019b1eb54a518d48ad54618fe78af
TAKU415 <th.mzk415@gmail.com>
2022-10-07 10:58:41 +0900

    aaa

--- Project/GameProject/Game02/UI02.cpp
+++ Project/GameProject/Game02/UI02.cpp
@@ -15,26 +15,11 @@ void UI02::Draw() {
 		m_img.Draw();
 	}
 
-	int time = GameData02::t_time / 60;
-	int t = 0;
-	for (int i = 0; i < 4; i++) {
-		if (i < 1) {
-			t = time % 10;
-			time /= 10;
-		}
-		else if (i < 2) {
-			t = time % 6;
-		}
-		else if (i < 3) {
-			t = time / 6;
-			time /= 6;
-		}
-		else {
-			t = time / 10;
-		}
-		m_img.SetRect(16 * t, 16, 16 * t + 16, 32);
+	int time = GameData02::t_time;
+	for (int i = 0; i < 8; i++, score /= 10) {
+		int s = score % 10;
 		m_img.SetSize(16, 16);
-		m_img.SetPos(200 - 16 * i, 0);
+		m_img.SetPos(0, 0);
 		m_img.Draw();
 	}
 }
